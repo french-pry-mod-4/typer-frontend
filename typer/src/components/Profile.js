562789beb711427c20586da704b260b348962b2f
“dinadeljanin” <ddeljanin5@icloud.com>
2020-07-28 01:03:57 -0400

    tackling the profile container

--- typer/src/components/Profile.js
+++ typer/src/components/Profile.js
@@ -4,8 +4,9 @@ export default class Profile extends Component {
   render() {
     return (
       <div className="content">
-        <h1>Here's your profile</h1>
-        <p>This will show all of your stats</p>
+        <div className="profile-header">
+          {/* structuring to add here */}
+        </div>
       </div>
     )
   }
