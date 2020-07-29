351a63896f4682339cfd43672f0a260b39bb84fc
“dinadeljanin” <ddeljanin5@icloud.com>
2020-07-28 21:37:17 -0400

    delete functionality is setup

--- typer/src/components/Profile.js
+++ typer/src/components/Profile.js
@@ -19,25 +19,37 @@ export default class Profile extends Component {
     })
   }
 
+  handleDelete = (id) => {
+    fetch(`http://localhost:3000/games/${id}`, {
+      method: 'DELETE',
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+  console.log('deleted')
+  }
+
   render() {
     const calcWPM = this.state.games.reduce((total, game) => total + game.speed, 0)
     const calcAccuracy = this.state.games.reduce((total, game) => total + game.accuracy, 0)
     return (
       <div className="content">
-        <div className="profile-header">
-          <h1 className="profile-userName">{this.state.user}</h1>
-          <div className="profile-userStats">
-            <div className="statsDataContainer">
-              <h2>Games</h2>
-              <h3>{this.state.games.length}</h3>
-            </div>
-            <div className="statsDataContainer">
-              <h2>WPM</h2>
-              <h3>{parseInt(calcWPM / this.state.games.length)}</h3>
-            </div>
-            <div className="statsDataContainer">
-              <h2>Accuracy</h2>
-              <h3>{parseInt(calcAccuracy / this.state.games.length)}</h3>
+        <div className="profile-header-wrapper">
+          <div className="profile-header">
+            <h1 className="profile-userName">{this.state.user}</h1>
+            <div className="profile-userStats">
+              <div className="statsDataContainer">
+                <h2>Games</h2>
+                <h3>{this.state.games.length}</h3>
+              </div>
+              <div className="statsDataContainer">
+                <h2>WPM</h2>
+                <h3>{parseInt(calcWPM / this.state.games.length)}</h3>
+              </div>
+              <div className="statsDataContainer">
+                <h2>Accuracy</h2>
+                <h3>{parseInt(calcAccuracy / this.state.games.length)}%</h3>
+              </div>
             </div>
           </div>
         </div>
@@ -47,15 +59,16 @@ export default class Profile extends Component {
               <th>Passage</th>
               <th>Speed</th>
               <th>Accuracy</th>
-              <th></th>
+              <th>Delete</th>
             </tr>
+            {/* create a div here that you can scroll through */}
             {this.state.games.map((game) =>
-                <tr>
+                <tr className="tableRow-data" id={game.id}>
                   <td>{game.id}</td>
                   <td>{game.speed}</td>
-                  <td>{game.accuracy}</td>
+                  <td>{game.accuracy}%</td>
                   {/* should only show up on hover */}
-                  <td><button>Delete</button></td>
+                  <td><button onClick={() => this.handleDelete(game.id)}>Delete</button></td>
                 </tr>
               )}
           </table>
